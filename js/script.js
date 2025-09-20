0efc64cef5ab49a760010c67e767f0d6f0dcb619
parksangseok <park0157@naver.com>
2025-09-20 22:49:07 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 
 $(function () {
 
+  //선택박스
+  $('.customSelectBox .btn').click(function () {
+
+    $(this).toggleClass('active');
+
+  });
+
+  $('.customSelectBox .selectList li .selectBtn').click(function () {
+
+    const text = $(this).text();
+    $(this).parents('.customSelectBox').children('.btn').text(text).removeClass('active');
+
+  });
+
 });
